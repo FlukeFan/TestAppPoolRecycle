3381834fb17492618b419abbf0dd30814e09be44
Richard Brown <fluke_fan@hotmail.com>
2021-01-02 16:54:08 +0000

    Added empty script to create a web app.

---
+++ prompt.ps1
@@ -0,0 +1,17 @@
+
+$thisScript = $MyInvocation.MyCommand.Path
+$thisScriptFolder = Split-Path -Parent $thisScript
+Set-Location $thisScriptFolder
+
+# force admin prompt
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+if (!$myWindowsPrincipal.IsInRole($adminRole)) {
+    Start-Process powershell -Verb runAs -ArgumentList "-noexit -file ""$thisScript"""
+    [System.Environment]::Exit(0)
+}
+
+$Host.UI.RawUI.WindowTitle = 'Prompt (Admin)'
+
+echo ""
