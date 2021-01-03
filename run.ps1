94d8b19a4bd11bfe193bacdb5fc8a76e051b7585
Richard Brown <fluke_fan@hotmail.com>
2021-01-03 10:27:23 +0000

    Changed timeout to 13 seconds to highlight timings.

--- run.ps1
+++ run.ps1
@@ -11,6 +11,7 @@ dotnet publish RecycleApp
 # create a website where the pool has disabled overlapping rotation
 $appPool = New-WebAppPool -Name TestAppPoolRecycle
 $appPool.recycling.disallowOverlappingRotation = "true"
+$appPool.processModel.shutdownTimeLimit = [TimeSpan]::FromSeconds(13)
 $appPool | Set-Item
 New-Website -Name TestAppPoolRecycle -ApplicationPool TestAppPoolRecycle -Port 8089 -PhysicalPath "$(Get-Location)\RecycleApp\bin\Debug\netcoreapp3.1\publish"
 
