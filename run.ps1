1bd02ef6dd813ee6edf63dac59b76ed02d9d1ce9
Richard Brown <fluke_fan@hotmail.com>
2021-01-03 10:10:42 +0000

    Added timings after recycle.

--- run.ps1
+++ run.ps1
@@ -13,3 +13,17 @@ $appPool = New-WebAppPool -Name TestAppPoolRecycle
 $appPool.recycling.disallowOverlappingRotation = "true"
 $appPool | Set-Item
 New-Website -Name TestAppPoolRecycle -ApplicationPool TestAppPoolRecycle -Port 8089 -PhysicalPath "$(Get-Location)\RecycleApp\bin\Debug\netcoreapp3.1\publish"
+
+# measure startup
+$startupTime = Measure-Command -Expression { Invoke-WebRequest -URI http://localhost:8089 -TimeoutSec 120 }
+Write-Host "Took $($startupTime.TotalMilliseconds)ms to startup"
+
+# recycle the pool
+Restart-WebAppPool TestAppPoolRecycle
+
+# if this is taken out, then the site fails with "503.0 - Service Unavailable" - which doesn't seem correct?
+Start-Sleep -Milliseconds 200
+
+# measure time after recycle
+$startupAfterRecycleTime = Measure-Command -Expression { Invoke-WebRequest -URI http://localhost:8089 -TimeoutSec 120 }
+Write-Host "Took $($startupAfterRecycleTime.TotalMilliseconds)ms to startup after recycle"
