b178a6286de9d0f433c5eae01f0603c493167fa6
Richard Brown <fluke_fan@hotmail.com>
2021-01-03 10:01:22 +0000

    Added website.

--- run.ps1
+++ run.ps1
@@ -4,8 +4,12 @@ Remove-Website -Name TestAppPoolRecycle -ErrorAction Ignore
 Remove-WebAppPool -Name TestAppPoolRecycle -ErrorAction Ignore
 Remove-Item RecycleApp -Recurse -ErrorAction Ignore
 
+# build a default app
 dotnet new web -o RecycleApp
 dotnet publish RecycleApp
+
+# create a website where the pool has disabled overlapping rotation
 $appPool = New-WebAppPool -Name TestAppPoolRecycle
 $appPool.recycling.disallowOverlappingRotation = "true"
 $appPool | Set-Item
+New-Website -Name TestAppPoolRecycle -ApplicationPool TestAppPoolRecycle -Port 8089 -PhysicalPath "$(Get-Location)\RecycleApp\bin\Debug\netcoreapp3.1\publish"
