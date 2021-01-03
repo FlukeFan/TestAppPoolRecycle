6be8d12671a7eb1ffd6da4089ef17b14d3e22b76
Richard Brown <fluke_fan@hotmail.com>
2021-01-03 09:50:40 +0000

    Added app pool.

--- run.ps1
+++ run.ps1
@@ -1,7 +1,11 @@
 
 # Clean up
 Remove-Website -Name TestAppPoolRecycle -ErrorAction Ignore
+Remove-WebAppPool -Name TestAppPoolRecycle -ErrorAction Ignore
 Remove-Item RecycleApp -Recurse -ErrorAction Ignore
 
 dotnet new web -o RecycleApp
 dotnet publish RecycleApp
+$appPool = New-WebAppPool -Name TestAppPoolRecycle
+$appPool.recycling.disallowOverlappingRotation = "true"
+$appPool | Set-Item
