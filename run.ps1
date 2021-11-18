657d65a5cdaba24bfe34bea5e9337fbea19646e2
Richard Brown <fluke_fan@hotmail.com>
2021-11-18 11:02:26 +0000

    Removed sleep.

--- run.ps1
+++ run.ps1
@@ -28,7 +28,7 @@ For ($i=0; $i -le 10; $i++) {
     Restart-WebAppPool TestAppPoolRecycle
 
     # without sleep, the site fails with "503.0 - Service Unavailable" - which doesn't seem correct?
-    Start-Sleep -milliseconds 200
+    # Start-Sleep -milliseconds 200
 
     # measure time after recycle
     $startupAfterRecycleTime = Measure-Command -Expression { Invoke-WebRequest -URI http://localhost:8089 -TimeoutSec 120 }
